412489db2082a97b3192197cc25e50406ea2791f
Jack Xiao <jack.xiao@xtracta.com>
2025-11-25 18:13:41 +1300

    Initial Commit

---
+++ ETPattern/ETPatternApp.swift
@@ -0,0 +1,21 @@
+//
+//  ETPatternApp.swift
+//  ETPattern
+//
+//  Created by admin on 25/11/2025.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct ETPatternApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ ETPatternTests/ETPatternTests.swift
@@ -0,0 +1,17 @@
+//
+//  ETPatternTests.swift
+//  ETPatternTests
+//
+//  Created by admin on 25/11/2025.
+//
+
+import Testing
+@testable import ETPattern
+
+struct ETPatternTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
