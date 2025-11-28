a25a8377a42711edbbbc215a44b6c0a3a91715d9
Jack Xiao <jack.xiao@xtracta.com>
2025-11-29 01:28:19 +1300

    Add attributes for review statistics in Card entity; refactor Card model to use properties for timesReviewed, timesCorrect, and lastReviewedDate

--- ETPattern/Models/Card.swift
+++ ETPattern/Models/Card.swift
@@ -14,37 +14,9 @@ public class Card: NSManagedObject {
 }
 
 extension Card {
-    // Computed properties for card statistics
-    var timesReviewed: Int32 {
-        get {
-            return self.value(forKey: "timesReviewed") as? Int32 ?? 0
-        }
-        set {
-            self.setValue(newValue, forKey: "timesReviewed")
-        }
-    }
-
-    var timesCorrect: Int32 {
-        get {
-            return self.value(forKey: "timesCorrect") as? Int32 ?? 0
-        }
-        set {
-            self.setValue(newValue, forKey: "timesCorrect")
-        }
-    }
-
     var successRate: Double {
         guard timesReviewed > 0 else { return 0.0 }
         return Double(timesCorrect) / Double(timesReviewed)
-    }
-
-    var lastReviewedDate: Date? {
-        get {
-            return self.value(forKey: "lastReviewedDate") as? Date
-        }
-        set {
-            self.setValue(newValue, forKey: "lastReviewedDate")
-        }
     }
 
     func recordReview(correct: Bool) {
